a27a8b2db747444c0b22b9e1d966c8e991886afa
erickarojas18 <125708318+erickarojas18@users.noreply.github.com>
2024-12-01 10:24:59 -0600

    MCV

--- app/Views/admin/lista.php
+++ app/Views/admin/lista.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
     <title><?= esc($title) ?></title>
 </head>
@@ -89,5 +90,8 @@
             <p>No se encontraron registros en la tabla amigos.</p>
         <?php endif; ?>
     </div>
+     <!-- jQuery and Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
